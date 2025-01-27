85d9b053910388f7a9b23a48d4c8c93717e09278
Prajwal Nagrale <prajwalnagrale@gmail.com>
2025-01-27 19:07:51 +0530

    User class updated

--- UserService/src/main/java/com/cg/users/entity/User.java
+++ UserService/src/main/java/com/cg/users/entity/User.java
@@ -1,5 +1,53 @@
 package com.cg.users.entity;
 
-public class User {
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="user_details")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	
+	private String email;
+	
+	private String  password;
+	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
 }
